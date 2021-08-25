c4e453368257d6ed1a4572731b326bc40afe96fb
simigeum <ldu50@naver.com>
2021-08-25 21:13:40 +0900

    🎈Array useState and Data-binding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let data = '안녕하세요';
+let [postName, b] = useState( ['How you like that 가사 해석', 'Kill this love', 'lovesick girl'] );
 
   return (
     <div className="App">
       <header className="gnb-header">
-        <div className="logo">금심이 Blog</div>
-        <div>{data}</div>
+        <h1 className="logo">금심이 Blog</h1>
       </header>
+      <ol className="post-list">
+        <li className="post-item">
+          <h3>{ postName[0] }</h3>
+          <span>2021.08.25</span>
+        </li>
+        <li className="post-item">
+          <h3>{ postName[1] }</h3>
+          <span>2021.08.21</span>
+        </li>  
+        <li className="post-item">
+          <h3>{ postName[2] }</h3>
+          <span>2021.08.14</span>
+        </li>
+      </ol>
     </div>
   );
 }
