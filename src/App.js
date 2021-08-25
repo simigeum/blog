04b0d0592ede88416258cd6f609849ad969197e2
simigeum <ldu50@naver.com>
2021-08-25 18:32:12 +0900

    🎈JSX and Data-binding

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let data = '안녕하세요';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="gnb-header">
+        <div className="logo">금심이 Blog</div>
+        <div>{data}</div>
       </header>
     </div>
   );
