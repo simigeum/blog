d2d8036c4e87b0b2ceae990b112be9e608f7e4b4
simigeum <ldu50@naver.com>
2021-08-27 00:55:15 +0900

    🎈 Setstate by deep copy and spread operator

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import './App.css';
 
 function App() {
 
-let [postName, b] = useState( ['How you like that 가사 해석', 'Kill this love', 'lovesick girl'] );
+let [postName, postNamefunction] = useState( ['How you like that 가사 해석', 'Kill this love', 'lovesick girl'] );
 let [like, likePlus] = useState(0);
+
+function postNameChange (){
+  var newArray = [...postName];
+  newArray[0] = 'Ice cream 가사 해석';
+  postNamefunction( newArray );
+}
+
 
   return (
     <div className="App">
       <header className="gnb-header">
         <h1 className="logo">금심이 Blog</h1>
       </header>
+      <button type="button" onClick={ postNameChange }>Ice cream</button>
       <ol className="post-list">
         <li className="post-item">
-          <h3>{ postName[0] } <span onClick={ ()=>{likePlus(like + 1)} }>😊</span> {like} </h3>
+          <h3>{ postName[0] } <span onClick={ () => { likePlus(like + 1) } }>😊</span> {like} </h3>
           <span>2021.08.25</span>
         </li>
         <li className="post-item">
